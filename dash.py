4b6d864b5c14be11d647ae6fba2e1fc26efbb87d
Shreemun <shreemun@gmail.com>
2024-12-26 06:29:51 +0530

    Update dash.py
    correct code

--- dash.py
+++ dash.py
@@ -1,173 +1,150 @@
 import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 import pandas as pd
+import plotly.graph_objs as go
 import plotly.express as px
-import plotly.graph_objects as go
-from dash import Dash, Input, Output, callback, dcc, html, no_update
 
-# Read the wildfire data into pandas dataframe
-df = pd.read_csv(
-    "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/Historical_Wildfires.csv"
-)
-# Extract year and month from the date column
-df["Month"] = pd.to_datetime(
-    df["Date"]
-).dt.month_name()  # used for the names of the months
-df["Year"] = pd.to_datetime(df["Date"]).dt.year
+data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/historical_automobile_sales.csv')
 
+app = dash.Dash(__name__)
 
-# Create app
+app.title = "Automobile Statistics Dashboard"
 
-# add tailwind to make styling easier later on
-external_scripts = [{"src": "https://cdn.tailwindcss.com"}]
-app = Dash(__name__, external_scripts=external_scripts)
-# Clear the layout and do not display exception till callback gets executed
-app.config.suppress_callback_exceptions = True
+dropdown_options = [
+    {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
+    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
+]
 
+year_list = [i for i in range(1980, 2024, 1)]
 
-# Layout Section of Dash
-
-app.layout = html.Div(
-    children=[
-        html.H1(
-            "Australia Wildfire Dashboard",
-            className="mt-8 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl",
-        ),
-        html.Div(
-            [
-                html.Label(
-                    "Regions: ", className="text-base font-semibold text-gray-900"
-                ),
-                html.P(
-                    "Which region would you like to display?",
-                    className="text-sm text-gray-500",
-                ),
-                html.Fieldset(
-                    children=[
-                        html.Legend("Selected Region", className="sr-only"),
-                        html.Div(
-                            children=[
-                                dcc.RadioItems(
-                                    [
-                                        {
-                                            "label": "New South Wales",
-                                            "value": "NSW",
-                                        },
-                                        {
-                                            "label": "Northern Territory",
-                                            "value": "NT",
-                                        },
-                                        {
-                                            "label": "Queensland",
-                                            "value": "QL",
-                                        },
-                                        {
-                                            "label": "South Australia",
-                                            "value": "SA",
-                                        },
-                                        {
-                                            "label": "Tasmania",
-                                            "value": "TA",
-                                        },
-                                        {
-                                            "label": "Victoira",
-                                            "value": "VI",
-                                        },
-                                        {
-                                            "label": "Western Australia",
-                                            "value": "WA",
-                                        },
-                                    ],
-                                    "NSW",
-                                    id="input-region",
-                                    className="flex items-center radio-btns flex-wrap",
-                                    inline=True,
-                                )
-                            ],
-                            className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0",
-                        ),
-                    ],
-                    className="mt-4",
-                ),
+app.layout = html.Div([
+    html.H1("Automobile Sales Statistics Dashboard",style={'textAlign':'center','color':'#503D36','font-size':24}),#May include style for title
+    html.Div([
+        html.Label("Select Statistics:"),
+        dcc.Dropdown(
+           id='dropdown-statistics',
+            options=[
+                {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
+                {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
             ],
-            className="mt-4",
+            value='Select Statistics',
+            placeholder='Select a report type',style={'textAlign':'center','width':'80%','padding':'3px','font-size':20}
+        )
+    ]), 
+
+    html.Div(dcc.Dropdown(
+            id='select-year',
+            options=[{'label': i, 'value': i} for i in year_list],
+            placeholder='Select Year',
+                  style={'textAlign':'center','width':'80%','padding':'3px','font-size':20})
+            
         ),
-        html.Div(
-            [
-                html.Label(
-                    "Year:",
-                    className="text-base font-semibold text-gray-900",
-                    htmlFor="year",
-                ),
-                html.P(
-                    "Which year would you like to display?",
-                    className="text-sm text-gray-500",
-                ),
-                dcc.Dropdown(df.Year.unique(), value=2005, id="input-year"),
-            ],
-            className="mt-4",
-        ),
-        html.Div(
-            [dcc.Graph(id="pie-plot"), dcc.Graph(id="bar-plot")],
-            className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2",
-        ),
-    ],
-    className="flex flex-col items-center",
-)
+
+    html.Div([
+    html.Div(id='output-container', className='chart-grid', style={'display':'flex'}),])
+])
+
+@app.callback(
+    Output(component_id='select-year', component_property='disabled'),
+    Input(component_id='dropdown-statistics',component_property='value'))
+
+def update_input_container(selected_statistics):
+    if selected_statistics =='Yearly Statistics': 
+        return False
+    else: 
+        return True
 
 
-@callback(
-    [
-        Output(component_id="pie-plot", component_property="figure"),
-        Output(component_id="bar-plot", component_property="figure"),
-    ],
-    [
-        Input(component_id="input-region", component_property="value"),
-        Input(component_id="input-year", component_property="value"),
-    ],
-)
-def get_graph(entered_region, entered_year):
-    up_df = df[(df["Year"] == int(entered_year)) & (df["Region"] == (entered_region))]
-
-    month_order = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ]
-    updf_pie = (
-        up_df[["Estimated_fire_area", "Month"]].groupby("Month").mean().reset_index()
-    )
-
-    pie_fig = px.pie(
-        updf_pie,
-        values="Estimated_fire_area",
-        names="Month",
-        category_orders={
-            "Month": month_order,
-        },
-        title=f"{entered_region}: Average Estimated Fire Area by Month in year {entered_year}",
-    )
-
-    updf_bar = up_df[["Count", "Month"]].groupby("Month").mean()
-    updf_bar = updf_bar.reindex(month_order, axis=0).reset_index()
-    bar_fig = px.bar(
-        updf_bar,
-        x="Month",
-        y="Count",
-        color_discrete_sequence=["#C45A9A"],
-        title=f"{entered_region}: Average Count of Pixels for Presumed Vegetation Fires in year {entered_year}",
-    )
-
-    return [pie_fig, bar_fig]
+@app.callback(
+    Output(component_id='output-container', component_property='children'),
+    [Input(component_id='dropdown-statistics', component_property='value'), 
+     Input(component_id='select-year', component_property='value')])
 
 
-if __name__ == "__main__":
-    app.run_server()
+def update_output_container(selected_statistics,input_year):
+    if selected_statistics == 'Recession Period Statistics':
+        recession_data = data[data['Recession'] == 1]
+        
+        yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        R_chart1 = dcc.Graph(
+            figure=px.line(yearly_rec, 
+                x='Year',
+                y='Automobile_Sales',
+                title="Average Automobile Sales fluctuation over Recession Period"))
+
+        average_sales = recession_data.groupby(['Year','Vehicle_Type'])['Automobile_Sales'].mean().reset_index()                           
+        R_chart2  = dcc.Graph(
+            figure=px.bar(average_sales,
+            x='Year',
+            y='Automobile_Sales',
+            color='Vehicle_Type',
+            title='Average Automobile Sales by Vehicle Type over Recession Period'
+        )
+        )
+        exp_rec=recession_data.groupby('Advertising_Expenditure').sum().reset_index()
+        R_chart3 = dcc.Graph(
+        figure=px.pie(exp_rec,
+        values = 'Recession',
+        names = 'Vehicle_Type',
+        title='Total Advertising Expenditure Share by Vehicle Type during Recessions'
+        )
+        )
+
+        unemp_rate = recession_data.groupby(['Vehicle_Type','Automobile_Sales'])['unemployment_rate'].sum().reset_index()
+        R_chart4  = dcc.Graph(
+            figure=px.bar(unemp_rate,
+            x='Vehicle_Type',
+            y='Automobile_Sales',
+            color='unemployment_rate',
+            title='Effect of Unemployment Rate on Vehicle Type and Sales'
+            )
+        )
+
+        return [
+            html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)]),
+            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)])
+            ]
+                           
+    elif (input_year and selected_statistics=='Yearly Statistics') :
+        yearly_data = data[data['Year'] == 0 ]
+                              
+        yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        Y_chart1 = dcc.Graph(figure=px.line(yas, 
+        x='Year',
+        y='Automobile_Sales',
+        title="Yearly Automobile Sales"
+        )
+            )
+            
+        mon_sales = data.groupby('Month')['Automobile_Sales'].mean().reset_index()
+        Y_chart2 = dcc.Graph(figure=px.line(mon_sales, 
+            x='Month',
+            y='Automobile_Sales',
+            title="Monthly Automobile Sales"
+        ))
+
+        avr_vdata=yearly_data.groupby(['Month','Vehicle_Type'])['Automobile_Sales'].mean().reset_index() 
+        Y_chart3 = dcc.Graph(figure=px.bar(avr_vdata,
+            x='Month',
+            y='Automobile_Sales',
+            color='Vehicle_Type',title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
+
+        exp_data=yearly_data.groupby('Advertising_Expenditure').sum().reset_index()
+        Y_chart4 = dcc.Graph(figure=px.pie(exp_data,
+            values = 'Advertising_Expenditure',
+            names = 'Vehicle_Type',
+            title='Total Advertisement Expenditure for each Vehicle Type in the year {}'.format(input_year))
+        )
+
+        return [
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)]),
+            html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)])
+                ]
+        
+    else:
+        return None
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
